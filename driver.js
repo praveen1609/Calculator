0d70cb03a44319685ad3442ef4d57de5a7b49cd7
praveen1609 <sushilmishrag3@gmail.com>
2021-03-18 12:59:24 +0530

    Equal works,functionally perfect now

--- driver.js
+++ driver.js
@@ -37,11 +37,11 @@ const numpad = document.querySelector('#CalDisp');
 const numb = document.querySelectorAll('#num button');
 const oper = document.querySelectorAll('#ope button');
 const cle = document.querySelector('#cls');
+const eq = document.querySelector('#equal')
 
-let opclick = 0;
-let op1,op2,tempop;
-let temp1 = 0;
-let temp2 = 0;
+let numarr = [];
+let oparr = [];
+let result = 0;
 
 cle.addEventListener('click',fresh);
 
@@ -53,14 +53,13 @@ oper.forEach((ope) => {
    ope.addEventListener('click', analyse);
 });
 
+eq.addEventListener('click',finalize);
+
 function fresh()
 {
-    temp1=0;
-    temp2=0;
-    opclick=0;
-    op1='';
-    op2='';
-    tempop='';
+    numarr = [];
+    oparr = [];
+    result = 0;
     numpad.value='';
     resu.value ='';
 }
@@ -68,27 +67,44 @@ function fresh()
 function MakeaNum()
 {
     numpad.value += this.value;
+    let numval=numpad.value;
+    console.log(JSON.stringify({numval}));
 }
 
 
 function analyse()
 {
-    opclick++;
- if (opclick % 2)
-   {
-     temp1 = Number(numpad.value);
-     numpad.value = '';
-
-     op1 = this.value;
-     //result= operate (,,)
-   }
-else
-{
-temp2 = Number(numpad.value);
-op2 = this.value;
-result = operate (op1,temp1,temp2);
-resu.value = result;
-numpad.value = '';
+    if(numpad.value != '')
+    numarr.push(Number(numpad.value));
+    console.log(JSON.stringify({numarr}));
+    oparr.push(this.value);
+    console.log(JSON.stringify({oparr}));
+    numpad.value = '';
+    if (numarr.length < 2)
+    return;
+    else
+    {
+        result = operate(oparr[0],numarr[0],numarr[1]);
+        console.log(JSON.stringify({result}));
+        numpad.value = '';
+        resu.value = result;
+        numarr.shift();
+        numarr.shift();
+        console.log(JSON.stringify({numarr}));
+        numarr.push(result);
+        console.log(JSON.stringify({numarr}));
+        oparr.shift();
+        console.log(JSON.stringify({oparr}));
+    }
 }
+
+function finalize()
+{
+    result = operate(oparr[0],numarr[0],Number(numpad.value));
+    numarr.shift();
+    numarr.push(result);
+    oparr.shift();
+    numpad.value = '';
+    resu.value = result;
 }
 
